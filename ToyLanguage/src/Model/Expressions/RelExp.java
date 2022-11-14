da2f54810f5b24b9dbbf6b6d72f7b63c1e65aa62
Cantor Alexandru <alexcantor64@gmail.com>
2022-11-15 00:40:03 +0200

    Implement RelExp

---
+++ ToyLanguage/src/Model/Expressions/RelExp.java
@@ -0,0 +1,54 @@
+package Model.Expressions;
+
+import Exceptions.MyException;
+import Model.ADT.Dictionary.MyIDictionary;
+import Model.Types.BoolType;
+import Model.Types.IntType;
+import Model.Values.BoolValue;
+import Model.Values.IntValue;
+import Model.Values.Value;
+
+public class RelExp implements Exp{
+
+    Exp e1;
+    Exp e2;
+    String operand;
+
+    public RelExp(String operand, Exp e1, Exp e2) {
+        this.operand = operand;
+        this.e1 = e1;
+        this.e2 = e2;
+    }
+    @Override
+    public Value eval(MyIDictionary<String, Value> tbl) throws MyException {
+        Value v1 = this.e1.eval(tbl);
+        Value v2 = this.e2.eval(tbl);
+        IntType intType = new IntType();
+
+        if(intType.equals(v1.getType()) && intType.equals(v2.getType())) {
+            IntValue i1 = (IntValue) v1;
+            IntValue i2 = (IntValue) v2;
+            return switch (operand) {
+                case "<" -> new BoolValue(i1.getVal() < i2.getVal());
+                case "<=" -> new BoolValue(i1.getVal() <= i2.getVal());
+                case "==" -> new BoolValue(i1.getVal() == i2.getVal());
+                case ">=" -> new BoolValue(i1.getVal() >= i2.getVal());
+                case ">" -> new BoolValue(i1.getVal() > i2.getVal());
+                case "!=" -> new BoolValue(i1.getVal() != i2.getVal());
+                default -> throw new MyException("Wrong relational operand type");
+            };
+        }
+        else {
+            throw new MyException("Not int types in relational expression.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "LogicExp{" +
+                "e1=" + e1 +
+                ", op=" + operand +
+                ", e2=" + e2 +
+                '}';
+    }
+}
